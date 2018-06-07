9340b70c1c284dcc003a9f729b21893afee00c36
bruno vial <bruno.vial@gmail.com>
2018-06-07 15:30:17 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pydeepltranslator',
+    version='0.1',
+    packages=['pydeepltranslator'],
+    url='',
+    license='',
+    author='bruno vial',
+    author_email='bruno.vial@gmail.com',
+    description='simple Python Api Wrapper for the deepl translation service'
+)
